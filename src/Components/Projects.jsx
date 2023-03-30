1e68daae3df23b8389b24bd02080809362c8872d
Rutu <113232070+Rutu-11@users.noreply.github.com>
2023-03-30 16:54:57 +0530

    project-2

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import {Box, Flex, Heading,Grid, GridItem, Text, Button , IconButton} from '@chakra-ui/react';
-import {BsGithub} from 'react-icons/bs'
+import {BsGithub} from 'react-icons/bs';
+import {AiOutlineEye} from 'react-icons/ai';
+
 function Projects() {
   return (
     <Box className='projects-container'>
@@ -10,7 +12,7 @@ function Projects() {
         <Grid templateColumns='repeat(1, 1fr)' gap={6} className='grid-container' h={'65%'} >
         <Box>
           <Box>
-            <img src={require("../utils/home1.png")} alt=""  />
+            <img src={require("../utils/home1.png")} alt="spotify"  />
             <Box className='project-info'>
                <Heading as={'h2'}>Spotify <span>Clone</span> </Heading>
                <Text>Spotify is a digital music service that gives you access to millions of songs.</Text>
@@ -27,29 +29,67 @@ function Projects() {
             </Box>
             {/* buttons */}
             <Box>
-            <IconButton
-            variant="outline"
-            colorScheme="orange"
-            aria-label="Send email"
-            icon={<BsGithub />}
-            onClick={() => {
-              window.open("https://github.com/Rutu-11", "_blank");
-            }}
-          />
+                <IconButton
+                variant="outline"
+                colorScheme="orange"
+                aria-label="Send email"
+                icon={<BsGithub />}
+                onClick={() => {
+                  window.open("https://github.com/Rutu-11/Spotify-clone", "_blank");
+                }}
+              />
+
+              <IconButton
+                variant="outline"
+                colorScheme="orange"
+                aria-label="Send email"
+                icon={<AiOutlineEye />}
+                onClick={() => {
+                  window.open("https://spotify-clone4me.netlify.app/", "_blank");
+                }}
+              />
             </Box>
         </Box>
 
 
         <Box>
-          <Box>
-            <img src="https://tse1.mm.bing.net/th?id=OIP.1YM53mG10H_U25iPjop83QHaEo&pid=Api&P=0" alt=""  />
+            <img src={require("../utils/Flipkart-clone.png")} alt="spotify"  />
             <Box className='project-info'>
                <Heading as={'h2'}>Spotify <span>Clone</span> </Heading>
-               <Text>Collaborative Project</Text>
-               <Text> Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, consequuntur. </Text>
+               <Text>Spotify is a digital music service that gives you access to millions of songs.</Text>
+               <Text>It is a collaborative project of four members completed in five days.</Text>
+               <Text><b>Features: </b> Login/Logout, SignUp, Search, Filter, Play, and Pause functionality has been implemented.</Text>
+               <Text><b>Techstack:</b> React, Redux, Chakra-UI, Styled-Component, Framer-motion</Text>
+               <Text><b>Areas of Responsibility:</b> 
+               
+               <li>Built a fully responsive Home page and Search page which closely resemble the user interface of Spotify.</li>
+               <li>Added filter functionality, page linking, and animations.</li>
+      
+               </Text>
             </Box>
+           
+            {/* buttons */}
+            <Box>
+                <IconButton
+                variant="outline"
+                colorScheme="orange"
+                aria-label="Send email"
+                icon={<BsGithub />}
+                onClick={() => {
+                  window.open("https://github.com/Rutu-11/Spotify-clone", "_blank");
+                }}
+              />
+
+              <IconButton
+                variant="outline"
+                colorScheme="orange"
+                aria-label="Send email"
+                icon={<AiOutlineEye />}
+                onClick={() => {
+                  window.open("https://spotify-clone4me.netlify.app/", "_blank");
+                }}
+              />
             </Box>
-            <h1>ii</h1>
         </Box>
 
         <Box>
